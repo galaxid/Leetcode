64c848d5ad64ef70167b369649dc40e44e0bb0d6
galaxid <41265670+galaxid@users.noreply.github.com>
2018-08-08 23:58:30 +0800

    Update 523.java

--- 523.java
+++ 523.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
         Set<Integer> set=new HashSet<>();
         for(int i:nums){
@@ -7,6 +7,25 @@ class Solution {
             if(set.contains(cur))return true;
             set.add(cur);
         }
+        return false;
+    }
+}*/
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        Set<Integer> set=new HashSet<>();
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            int cur;
+            sum+=nums[i];
+            if(k!=0){
+                cur=sum%k;
+                if(i>0){
+                    if(set.contains(cur)||cur==0&&sum!=0)return true;
+                }
+                set.add(cur);
+            }
+        }
+        if(sum==0&&nums.length>1)return true;
         return false;
     }
 }
